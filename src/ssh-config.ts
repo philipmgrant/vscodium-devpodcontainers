7540d595f8f345051231af93a4b1338b8f06b198
philipmgrant <46570855+philipmgrant@users.noreply.github.com>
2026-03-21 16:40:48 +0000

    If 'remote.SSH.configFile' is set, use it whenever we invoke ssh

--- src/ssh-config.ts
+++ src/ssh-config.ts
@@ -5,3 +5,15 @@ export function getSshConfigPath() {
     .getConfiguration("remote.SSH")
     .get<string>("configFile", "");
 }
+
+export function buildSshCommand(args: string[] = []) {
+  const configPath = getSshConfigPath();
+  let argArray = [...args];
+  if (configPath) {
+    argArray.unshift("-F", configPath);
+  }
+  return {
+    command: "ssh",
+    args: argArray
+  }
+}
